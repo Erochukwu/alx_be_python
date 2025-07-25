5aeb8c9593dc7f0c884e19ca6b995518813f0d31
Erochukwu <fortifiedmercy@gmail.com>
2025-07-25 23:31:00 +0100

    Add arithmetic operation file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,10 +9,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return "Error: Cannot divide by zero"
+        return num1 /num2
+    if num2 == 0:
+        return "Error: Cannot divide by zero"
 print(perform_operation (5, 8, "divide"))
     
     
