85930013361789751ecab21f666063f7b10f83b6
Erochukwu <fortifiedmercy@gmail.com>
2025-07-25 23:26:49 +0100

    Add arithmetic operation file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1, num2, operation):
+    if operation not in ["add", "subtract", "multiply", "divide"]:
+        return "Error: Invalid operation"
+    
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 -num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero"
+print(perform_operation (5, 8, "divide"))
+    
+    
