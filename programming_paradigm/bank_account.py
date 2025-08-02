7f098657508aead045d5b7f7387c689c6c96a660
Erochukwu <fortifiedmercy@gmail.com>
2025-08-02 23:14:58 +0100

    Bank account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __inti__(self, account_balance):
+        self.account_balance = 0
+
+    def deposit(self, amount):
+        if amount > self.account_balance:
+            self.account_balance = amount
+    
+        def withdraw(amount):
+            if amount <= self.account_balance:
+                self.account_balance -= amount
+                print("your new account balance is, {self.account_balance}")
+            else:
+                print("You do not have sufficient balance for this withdrawal")
+    
