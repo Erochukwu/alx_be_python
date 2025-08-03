0b5fcb3f838d4950d9a151880b6d7d68bf7ca429
Erochukwu <fortifiedmercy@gmail.com>
2025-08-03 17:45:03 +0100

    Test simple calculator file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,19 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(6, 3), 8)
         self.assertEqual(self.calc.add(15, 8), 23)
-        self.assertEqual(self.calc.add(15, three), 18)
+        self.assertEqual(self.calc.add(15, 3), 18)
+
+    def test_subtraction(self):
+        """Test the subtraction method"""
+        self.assertEqual(self.calc.subtract(6, 1), 3)
+        self.assertEqual(self.calc.subtract(3, 8), -5)
+        self.assertEqual(self.calc.subtract(17, 2), 15)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(6, 1), 6)
+        self.assertEqual(self.calc.multiply(4, 8), 30)
+        self.assertEqual(self.calc.multiply(10, 0), 0)
+        self.assertEqual(self.calc.multiply(6, 3), 6)
         # Add more assertions to thoroughly test the add method.
 
 # Remember to write additional test methods for subtract, multiply, and divide.
