aa2aa81655282d3ba7bcef5d7e3e9dddd5d49b5b
Erochukwu <fortifiedmercy@gmail.com>
2025-08-03 01:01:56 +0100

    Bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,11 +2,11 @@ class BankAccount:
     def __init__(self, balance=0.0):
         self.account_balance = balance
 
-    def deposit(self):
+    def deposit(self, amount):
         self.account_balance += amount
         print(f"Deposited: ${amount}")
     
-    def withdraw(self):
+    def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
             print(f"Withdrew: ${amount}")
