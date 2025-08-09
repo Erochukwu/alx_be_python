6d64d956c6f296d2672cf101c7f763bf54ecb868
Erochukwu <fortifiedmercy@gmail.com>
2025-08-09 08:46:18 +0100

    Add Book Class file

--- oop/book_class.py
+++ oop/book_class.py
@@ -14,4 +14,4 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        print(f"Deleting {self.year}")
+        print(f"Deleting {self.title}")
