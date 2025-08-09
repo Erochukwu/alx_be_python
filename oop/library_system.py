9983fdad7600493d7012877ed8cab659380c4ddd
Erochukwu <fortifiedmercy@gmail.com>
2025-08-09 15:26:03 +0100

    Add Library system file

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,12 +20,9 @@ class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book):
+    def add_book(self, book: Book):
         self.books.append(book)
 
     def list_books(self):
-        if not self.books:
-            print("No books in the library")
-        else:
-            for book in self.books:
+        for book in self.books:
                 print(book)
