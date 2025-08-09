47b2d59f6ef4c1b6cc23c86b099f4ca73cd000d7
Erochukwu <fortifiedmercy@gmail.com>
2025-08-09 08:42:57 +0100

    Add Book Class file

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,14 +2,10 @@ from enum import auto
 
 
 class Book:
-    def __init__(self, title(str), author(str), year(int)):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
-        self.author = author # type: ignore
-        self.year = year # type: ignore
-        print("(title) by (author), published in (year)")
-    
-    def __del__(self):
-        self.file.close()
+        self.author = author
+        self.year = year
 
     def __str__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
@@ -17,3 +13,5 @@ class Book:
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
+    def __del__(self):
+        print(f"Deleting {self.title}")
